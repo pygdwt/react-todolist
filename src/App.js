40da037b719c8cde55ad022c16b06d85a5dd107a
pygdwt <68355889+pygdwt@users.noreply.github.com>
2020-07-16 17:18:00 +0800

    Komponen Paper

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import React from "react";
 import "./styles.css";
 
+import Paper from "./components/Paper";
+
 export default function App() {
   return (
-    <div className="container">
-      <div className="frame">
-        <section className="header">
-          <button className="btn-header add-color">Add</button>
-          <h1 className="judul-header">ToDo List</h1>
-          <button className="btn-header clear-color">Clear</button>
-        </section>
+    <Paper>
+      <section className="header">
+        <button className="btn-header add-color">Add</button>
+        <h1 className="judul-header">ToDo List</h1>
+        <button className="btn-header clear-color">Clear</button>
+      </section>
 
-        <section className="add">
-          <form className="add-form">
-            <input type="text" className="add-input" />
-            <button className="add-btn">Add</button>
-          </form>
-        </section>
+      <section className="add">
+        <form className="add-form">
+          <input type="text" className="add-input" />
+          <button className="add-btn">Add</button>
+        </form>
+      </section>
 
-        <section className="todos">
-          <div className="todo">
-            <span className="todo-text">Sarapan</span>
-          </div>
-        </section>
-      </div>
-    </div>
+      <section className="todos">
+        <div className="todo">
+          <span className="todo-text">Sarapan</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">Makan Siang</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">Makan Malam</span>
+        </div>
+      </section>
+    </Paper>
   );
 }
