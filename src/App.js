87689b21f904022817669b3d8f9f07588af58085
pygdwt <68355889+pygdwt@users.noreply.github.com>
2020-07-16 15:00:05 +0800

    Migrasi HTML CSS ke React

--- src/App.js
+++ src/App.js
@@ -3,9 +3,27 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <div className="frame">
+        <section className="header">
+          <button className="btn-header add-color">Add</button>
+          <h1 className="judul-header">ToDo List</h1>
+          <button className="btn-header clear-color">Clear</button>
+        </section>
+
+        <section className="add">
+          <form className="add-form">
+            <input type="text" className="add-input" />
+            <button className="add-btn">Add</button>
+          </form>
+        </section>
+
+        <section className="todos">
+          <div className="todo">
+            <span className="todo-text">Sarapan</span>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
