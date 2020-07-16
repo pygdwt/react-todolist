ad6132dd9976d1beb36d5966e46eced9b440cec8
pygdwt <68355889+pygdwt@users.noreply.github.com>
2020-07-16 21:04:08 +0800

    membuat komponen Header, Todos, & Todo Form

--- src/App.js
+++ src/App.js
@@ -2,34 +2,16 @@ import React from "react";
 import "./styles.css";
 
 import Paper from "./components/Paper";
+import Header from "./components/Header";
+import TodoForm from "./components/TodoForm";
+import Todos from "./components/Todos";
 
 export default function App() {
   return (
     <Paper>
-      <section className="header">
-        <button className="btn-header add-color">Add</button>
-        <h1 className="judul-header">ToDo List</h1>
-        <button className="btn-header clear-color">Clear</button>
-      </section>
-
-      <section className="add">
-        <form className="add-form">
-          <input type="text" className="add-input" />
-          <button className="add-btn">Add</button>
-        </form>
-      </section>
-
-      <section className="todos">
-        <div className="todo">
-          <span className="todo-text">Sarapan</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Makan Siang</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Makan Malam</span>
-        </div>
-      </section>
+      <Header />
+      <TodoForm />
+      <Todos />
     </Paper>
   );
 }
