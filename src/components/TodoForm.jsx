a80f9f5d33679a35cad8080f90f90c1f05465d73
pygdwt <68355889+pygdwt@users.noreply.github.com>
2020-07-17 15:01:14 +0800

    Tombol Add Header Gak Fungsi nyed

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-const TodoForm = () => {
-  return (
-    <section className="add">
-      <form className="add-form">
-        <input type="text" className="add-input" />
-        <button className="add-btn">Add</button>
-      </form>
-    </section>
-  );
+const TodoForm = ({ addTodo, showAdd }) => {
+  const [value, setValue] = useState("");
+
+  const handleFormSubmit = e => {
+    e.preventDefault();
+
+    if (!value) {
+      alert("sorry, input cannot be blank");
+      return;
+    }
+
+    addTodo(value);
+    setValue("");
+  };
+  if (showAdd) {
+    return (
+      <section className="add">
+        <form className="add-form" onSubmit={handleFormSubmit}>
+          <input
+            type="text"
+            className="add-input"
+            value={value}
+            onChange={e => setValue(e.target.value)}
+          />
+          <button className="add-btn">Add</button>
+        </form>
+      </section>
+    );
+  } else {
+    return null;
+  }
 };
 
+TodoForm.propTypes = {
+  addTodo: PropTypes.func.isRequired,
+  showAdd: PropTypes.bool.isRequired
+};
 export default TodoForm;
