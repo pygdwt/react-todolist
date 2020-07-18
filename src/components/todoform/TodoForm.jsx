1663a68b74beb91ac50cd455730b021d1c698da1
Yogo <68355889+pygdwt@users.noreply.github.com>
2020-07-18 13:51:22 +0800

    membuat komponen TodoForm dengan Module CSS

---
+++ src/components/todoform/TodoForm.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import styles from "./todoform.module.css";
+
+const TodoForm = ({ addTodo, showAdd }) => {
+  const [value, setValue] = useState("");
+
+  const handleFormSubmit = e => {
+    e.preventDefault();
+
+    if (!value) {
+      alert("sorry, input cannot be blank");
+      return;
+    }
+
+    if (value.length > 50) {
+      alert("sorry, maximum input is 50");
+      setValue("");
+      return;
+    }
+
+    addTodo(value);
+    setValue("");
+  };
+  if (showAdd) {
+    return (
+      <section className={styles.add}>
+        <form className={styles.addForm} onSubmit={handleFormSubmit}>
+          <input
+            type="text"
+            className={styles.addInput}
+            value={value}
+            onChange={e => setValue(e.target.value)}
+          />
+          <button className={styles.addBtn}>Add</button>
+        </form>
+      </section>
+    );
+  } else {
+    return null;
+  }
+};
+
+TodoForm.propTypes = {
+  addTodo: PropTypes.func.isRequired,
+  showAdd: PropTypes.bool.isRequired
+};
+export default TodoForm;
