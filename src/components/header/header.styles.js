3811b28205d59aa2f75e910dea313eed8f338252
Yogo <68355889+pygdwt@users.noreply.github.com>
2020-07-19 11:05:57 +0800

    merefactor komponen Header dengan Props Emotion CSS

---
+++ src/components/header/header.styles.js
@@ -0,0 +1,18 @@
+import { css } from "@emotion/core";
+
+export const header = () =>
+  css`
+    display: flex;
+    align-items: flex-start;
+  `;
+
+export const judulHeader = () =>
+  css`
+    width: 52%;
+    text-align: center;
+
+    font-family: "Fredericka the Great";
+    font-size: 3.6rem;
+    text-transform: lowercase;
+    color: #0062d1;
+  `;
